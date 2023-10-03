e3cc300996300db223b2c988f124953d30d70ab2
bhawna-sinha <114169817+bhawna-sinha@users.noreply.github.com>
2023-10-03 17:42:42 +0530

    Added person_db_script

---
+++ Person/person_db_script.sql
@@ -0,0 +1,45 @@
+create table person (
+	id BIGSERIAL NOT NULL PRIMARY KEY,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	gender VARCHAR(50),
+	date_of_birth DATE,
+	country_of_birth VARCHAR(50)
+);
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Twila', 'Larmouth', 'tlarmouth0@goo.ne.jp', 'Female', '2023/06/11', 'Portugal');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Elsa', 'Itzhaki', 'eitzhaki1@tripadvisor.com', 'Female', '2023/01/09', 'Lithuania');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Eartha', 'Duligall', 'eduligall2@springer.com', 'Female', '2023/08/18', 'South Africa');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Cherlyn', 'Chevis', null, 'Female', '2023/04/15', 'China');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Keir', 'Yurkov', 'kyurkov4@mlb.com', 'Male', '2022/10/08', 'Russia');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Creighton', 'Cluderay', 'ccluderay5@answers.com', 'Male', '2023/08/17', 'United States');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Bette-ann', 'Spilsted', 'bspilsted6@technorati.com', 'Female', '2022/12/13', 'Greece');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Thomasine', 'Otton', 'totton7@princeton.edu', 'Female', '2023/07/10', 'Philippines');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Rosamond', 'McAreavey', 'rmcareavey8@blinklist.com', 'Female', '2023/03/02', 'Egypt');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Zahara', 'Zaniolo', 'zzaniolo9@cam.ac.uk', 'Female', '2022/10/07', 'China');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Pavlov', 'Sheffield', 'psheffielda@clickbank.net', 'Male', '2023/01/17', 'Finland');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Briny', 'Rozier', 'brozierb@cpanel.net', 'Female', '2023/07/23', 'Argentina');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Heall', 'McFetrich', 'hmcfetrichc@ebay.co.uk', 'Male', '2023/03/18', 'Tanzania');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Yorgo', 'Calan', null, 'Male', '2023/06/14', 'United States');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Reeba', 'Ottee', 'rotteee@bizjournals.com', 'Female', '2023/06/22', 'Russia');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Caria', 'Divis', 'cdivisf@xinhuanet.com', 'Female', '2023/02/24', 'China');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Woodrow', 'Van der Linde', 'wvanderlindeg@t.co', 'Male', '2023/06/16', 'Tajikistan');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Hadlee', 'Lorent', 'hlorenth@craigslist.org', 'Male', '2022/10/21', 'China');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Clem', 'Thornton-Dewhirst', 'cthorntondewhirsti@prlog.org', 'Bigender', '2023/09/12', 'Philippines');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Matthaeus', 'Oades', 'moadesj@webmd.com', 'Genderqueer', '2023/03/14', 'Finland');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Magdalene', 'Grisedale', 'mgrisedalek@hibu.com', 'Female', '2023/04/18', 'Luxembourg');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Renaud', 'Hugues', 'rhuguesl@webmd.com', 'Male', '2023/03/13', 'China');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Madlen', 'Serraillier', null, 'Female', '2022/12/31', 'Angola');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Lexine', 'Mearns', 'lmearnsn@nymag.com', 'Female', '2022/11/16', 'Greece');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Titos', 'Koschke', null, 'Male', '2023/07/24', 'Japan');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Valentino', 'Dimitriades', 'vdimitriadesp@blogger.com', 'Male', '2023/04/16', 'Japan');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Mathian', 'Partleton', null, 'Male', '2023/01/18', 'United States');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Carolee', 'Andrichak', 'candrichakr@t.co', 'Female', '2023/01/07', 'Indonesia');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Carney', 'Manthorpe', null, 'Bigender', '2023/09/03', 'France');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Kalil', 'Tournie', null, 'Male', '2023/04/24', 'Poland');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Sandi', 'McCoughan', 'smccoughanu@icq.com', 'Female', '2022/12/24', 'China');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Prudy', 'Sharma', 'psharmav@trellian.com', 'Genderfluid', '2022/12/05', 'Russia');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Isidora', 'Livett', null, 'Female', '2023/04/20', 'China');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Bridie', 'Peealess', 'bpeealessx@stanford.edu', 'Female', '2023/04/23', 'Mexico');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Patricio', 'Gleave', 'pgleavey@shinystat.com', 'Male', '2023/01/19', 'China');
+insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Anabal', 'Romayne', 'aromaynez@seattletimes.com', 'Female', '2022/12/31', 'Indonesia');
