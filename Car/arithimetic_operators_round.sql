13fbc92aecc6b4b89b48ff38db080f4fb814ac03
bhawna-sinha <114169817+bhawna-sinha@users.noreply.github.com>
2023-10-04 10:28:46 +0530

    Added arithmetic operator round

---
+++ Car/arithimetic_operators_round.sql
@@ -0,0 +1,19 @@
+arthimatic operators rounds
+
+10% of price
+SELECT id, make, model, price, price * 0.10 FROM car;
+
+round
+
+SELECT id, make, model, price, 
+ROUND(price * 0.10, 2) FROM car;
+
+discounted price with 10%
+
+SELECT id, make, model, price, 
+ROUND(price * 0.10, 2), 
+ROUND(price - ROUND(price * 0.10, 2)) FROM car;
+
+SELECT id, make, model, price AS original_price, 
+ROUND(price * 0.10, 2) AS ten_percent, 
+ROUND(price - ROUND(price * 0.10, 2)) AS discounted_price FROM car;
