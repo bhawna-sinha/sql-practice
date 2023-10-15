3b34e8948b2980124ec781be6829bd08ab412457
bhawna-sinha <114169817+bhawna-sinha@users.noreply.github.com>
2023-10-15 21:54:55 +0530

    added 1 more question in aggregation

--- question practice/Aggregation.sql
+++ question practice/Aggregation.sql
@@ -12,3 +12,9 @@ WHERE COUNTRYCODE = 'JPN';
 
 SELECT (MAX(population) - MIN(population))
 FROM CITY;
+
+
+4. Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+
+SELECT COUNT(*) - COUNT(DISTINCT CITY) AS CityCountDifference
+FROM STATION;
