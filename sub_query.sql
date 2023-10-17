32f9a5f453214bc9e432c8bc7dc0faa68ddde677
bhawna-sinha <114169817+bhawna-sinha@users.noreply.github.com>
2023-10-17 22:27:13 +0530

    Updated Sub_query

--- sub_query.sql
+++ sub_query.sql
@@ -10,12 +10,41 @@ insert into customer (customer_id, amount, mode, payment_date) values (3, 95640,
 insert into customer  (customer_id, amount, mode, payment_date) values (4, 197392, 'cash', '10-2-2022');
 insert into customer (customer_id, amount, mode, payment_date) values (5, 50682, 'online', '2-5-2023');
 
+sub querry
 
 FIND AVERAGE AMOUNT AND FILTER THE CUSTOMER WHOSE AMOUNT IS GREATER THAN AVERAGE AMOUNT
 
+1. avgerage amount-
 
-sub query/nested query
+(SELECT AVG(amount) FROM customer);
+
+2. sub query/nested query (using comparison operator)-
 
 SELECT *
 FROM customer
 WHERE amount > (SELECT AVG(amount) FROM customer);
+
+3. using logical operator-
+
+A. (in)
+
+SELECT customer_id, amount, mode 
+FROM customer
+WHERE customer_id IN (SELECT customer_id FROM cust1);
+
+B. (exists operator)
+
+SELECT name
+FROM cust1 
+WHERE EXISTS(SELECT customer_id, amount
+              FROM customer
+              WHERE cust1.id = customer.customer_id
+			  AND amount >10000);
+
+
+
+
+
+
+
+
